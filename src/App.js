91c8bfed6cd3dbee3cd629893b5a61850b24b5ac
gabriel-ved <gc.oliveiransantos@gmail.com>
2019-11-16 00:22:32 -0300

    Logica-game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Main from './pages/Main';
+import Jogo from './pages/Jogo';
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+        <Route exact path='/1' component={Jogo}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
